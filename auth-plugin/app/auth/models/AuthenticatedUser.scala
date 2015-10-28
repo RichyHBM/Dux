19fac3cfd994dc356c3c045126a1c7949eece6c6
RichyHBM <gitemail@richyhbm.co.uk>
2015-10-28 20:55:38 +0000

    Make AuthenticatedUser immutable

--- auth-plugin/app/auth/models/AuthenticatedUser.scala
+++ auth-plugin/app/auth/models/AuthenticatedUser.scala
@@ -3,11 +3,9 @@ package auth.models
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-import scala.beans.BeanProperty
-
-case class AuthenticatedUser(@BeanProperty var id: Long,
-                             @BeanProperty var name: String,
-                             @BeanProperty var email: String) {
+case class AuthenticatedUser(id: Long,
+                             name: String,
+                             email: String) {
 
   def toJson(): String = Json.stringify(Json.toJson(this))
 }
