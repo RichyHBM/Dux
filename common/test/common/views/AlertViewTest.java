1ab7ded75a4c37c1f50ccf6fffea37895442d2fb
RichyHBM <gitemail@richyhbm.co.uk>
2015-10-30 13:43:41 +0000

    Add navbar template and tests to assure views render correctly

---
+++ common/test/common/views/AlertViewTest.java
@@ -0,0 +1,38 @@
+package common.views;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import common.models.BasicViewResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.*;
+
+import play.mvc.*;
+import play.test.*;
+import play.data.DynamicForm;
+import play.data.validation.ValidationError;
+import play.data.validation.Constraints.RequiredValidator;
+import play.i18n.Lang;
+import play.libs.F;
+import play.libs.F.*;
+import play.twirl.api.Content;
+import play.twirl.api.Html;
+
+import static play.test.Helpers.*;
+import static org.junit.Assert.*;
+
+
+/**
+ * These tests make sure all the views compile as expected
+ */
+public class AlertViewTest {
+
+    @Test
+    public void alertViewTest() {
+        Content html = common.views.html.alert.render();
+
+        assertTrue(contentAsString(html).contains("AlertMessage"));
+    }
+}
