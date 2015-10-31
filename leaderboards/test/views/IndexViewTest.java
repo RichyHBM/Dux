7d6a25282184638504bbb2dc6a66d385eda94e1b
RichyHBM <gitemail@richyhbm.co.uk>
2015-10-31 19:04:28 +0000

    Submit empty json as view data

--- leaderboards/test/views/IndexViewTest.java
+++ leaderboards/test/views/IndexViewTest.java
@@ -35,11 +35,7 @@ public class IndexViewTest extends WithApplication {
     @Test
     public void indexViewNullUserTest() {
         BasicAuthViewResponse response = new BasicAuthViewResponse("Leaderboards", null);
-        ObjectNode viewData = Json.newObject();
-        viewData.put("TotalLeaderboards", 0);
-        viewData.put("ActiveLeaderboards", 0);
-
-        Content html = views.html.index.render(viewData, response);
+        Content html = views.html.index.render(Json.newObject(), response);
 
         assertTrue(contentAsString(html).contains(response.title()));
         assertTrue(contentAsString(html).contains("Sign In"));
@@ -48,11 +44,7 @@ public class IndexViewTest extends WithApplication {
     @Test
     public void indexViewWithUserTest() {
         BasicAuthViewResponse response = new BasicAuthViewResponse("Leaderboards", new AuthenticatedUser(1, "test", "test@test"));
-        ObjectNode viewData = Json.newObject();
-        viewData.put("TotalLeaderboards", 0);
-        viewData.put("ActiveLeaderboards", 0);
-
-        Content html = views.html.index.render(viewData, response);
+        Content html = views.html.index.render(Json.newObject(), response);
 
         assertTrue(contentAsString(html).contains(response.title()));
         assertTrue(contentAsString(html).contains("test"));
