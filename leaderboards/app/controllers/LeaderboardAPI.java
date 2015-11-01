24e8cdab774297beefe189bb046f5bffa3fb0fb6
RichyHBM <gitemail@richyhbm.co.uk>
2015-11-01 12:22:54 +0000

    Edit leaderboard times

--- leaderboards/app/controllers/LeaderboardAPI.java
+++ leaderboards/app/controllers/LeaderboardAPI.java
@@ -12,6 +12,7 @@ import play.mvc.Result;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class LeaderboardAPI extends Controller {
@@ -24,7 +25,7 @@ public class LeaderboardAPI extends Controller {
     public Result getLeaderboardsForGame() {
         JsonNode json = request().body().asJson();
 
-        if(json == null)
+        if (json == null)
             return badRequest("Game ID required");
 
         int gameId = json.findPath("gameId").asInt(-1);
@@ -36,12 +37,31 @@ public class LeaderboardAPI extends Controller {
 
     public Result getLeaderboardCount() {
         JsonNode json = request().body().asJson();
-        if(json == null)
+        if (json == null)
             return badRequest("Game ID and Leaderboard Name required");
 
         int gameId = json.findPath("gameId").asInt(-1);
         String leaderboardName = json.findPath("leaderboardName").asText("");
         Long count = provider.getUserCountInLeaderboard(gameId, leaderboardName);
         return ok(count.toString());
+    }
+
+    public Result editLeaderboard() {
+        JsonNode json = request().body().asJson();
+        if (json == null)
+            return badRequest("Game ID, Leaderboard Name, and data required");
+
+        int gameId = json.findPath("gameId").asInt(-1);
+        String leaderboardName = json.findPath("leaderboardName").asText("");
+
+        long startTime = json.findPath("startTime").asLong(-1);
+        long endTime = json.findPath("endTime").asLong(-1);
+
+        Date startDate = startTime == -1 ? null : new Date(startTime);
+        Date endDate = endTime == -1 ? null : new Date(endTime);
+
+        Boolean success = manager.editLeaderboard(gameId, leaderboardName, startDate, endDate);
+
+        return ok(Json.toJson(success));
     }
 }
