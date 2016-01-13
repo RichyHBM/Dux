3ed56b1a76aa745704ba4a6276e9c5be2666a758
RichyHBM <gitemail@richyhbm.co.uk>
2016-01-13 21:33:19 +0000

    Add apis for adding new group, app and permission

--- authentication/app/controllers/AuthenticationAPIv1.scala
+++ authentication/app/controllers/AuthenticationAPIv1.scala
@@ -3,6 +3,7 @@ package controllers
 import java.util.Date
 
 import com.fasterxml.jackson.databind.node.ObjectNode
+import models.view.{ViewApp, ViewPermission, ViewGroup}
 import models.{view, UserSession}
 import play.api._
 import play.api.mvc._
@@ -18,14 +19,18 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import common.models.BasicViewResponse
 import interfaces._
 
+import scala.concurrent.Future
+
 class AuthenticationAPIv1 @Inject()(cacheApi: CacheApi, authCache: IAuthenticationCache) extends Controller with AuthenticatedActionBuilder {
   def cache = cacheApi
 
-  def listAllLoggedIn = AuthenticatedAction(auth.AuthenticationType.None) {
+  val authType = auth.AuthenticationType.None
+
+  def listAllLoggedIn = AuthenticatedAction(authType) {
     Ok( Json.toJson( authCache.getAllLoggedIn().map(kv => kv._2) ) )
   }
 
-  def removeSession = AuthenticatedAction(auth.AuthenticationType.None) { request =>
+  def removeSession = AuthenticatedAction(authType) { request =>
     request.body.asJson match {
       case Some(json: JsValue) => {
         val user = UserSession.fromJson(json.toString())
@@ -37,27 +42,56 @@ class AuthenticationAPIv1 @Inject()(cacheApi: CacheApi, authCache: IAuthenticati
     }
   }
 
-  def getAllApps = AuthenticatedAction(auth.AuthenticationType.None).async {
+  def getAllApps = AuthenticatedAction(authType).async {
     Apps.listAll().map( l => {
       Ok( Json.toJson( l.map(a => view.ViewApp(a.Id, a.Name, a.Description)).toList ) )
     })
   }
 
-  def getAllGroups = AuthenticatedAction(auth.AuthenticationType.None).async {
+  def getAllGroups = AuthenticatedAction(authType).async {
     Groups.listAll().map( l => {
       Ok( Json.toJson( l.map(g => view.ViewGroup(g.Id, g.Name, g.Description)).toList ) )
     })
   }
 
-  def getAllPermissions = AuthenticatedAction(auth.AuthenticationType.None).async {
+  def getAllPermissions = AuthenticatedAction(authType).async {
     Permissions.listAll().map( l => {
       Ok( Json.toJson( l.map(p => view.ViewPermission(p.Id, p.Name, p.Description)).toList ) )
     })
   }
 
-  def getAllUsers = AuthenticatedAction(auth.AuthenticationType.None).async {
+  def getAllUsers = AuthenticatedAction(authType).async {
     Users.listAll().map( l => {
       Ok( Json.toJson( l.map(u => view.ViewUser(u.Id, u.Name, u.Email, u.ApiKey, u.CreatedOn.asInstanceOf[Date], u.FailedAttempts, u.Blocked)).toList ))
     })
+  }
+
+  def newGroup = AuthenticatedAction(authType).async(parse.json) { request =>
+    request.body.validate[ViewGroup].fold(
+      errors => {
+        Future { BadRequest(JsError.toJson(errors)) }
+      }, viewGroup => {
+        Groups.add(new Group(viewGroup.Name, viewGroup.Description)).map( i => Ok(i.toString) )
+      }
+    )
+  }
+
+  def newApp = AuthenticatedAction(authType).async(parse.json) { request =>
+    request.body.validate[ViewApp].fold(
+      errors => {
+        Future { BadRequest(JsError.toJson(errors)) }
+      }, viewApp => {
+        Apps.add(new App(viewApp.Name, viewApp.Description)).map( i => Ok(i.toString) )
+      }
+    )
+  }
+  def newPermission = AuthenticatedAction(authType).async(parse.json) { request =>
+    request.body.validate[ViewPermission].fold(
+      errors => {
+        Future { BadRequest(JsError.toJson(errors)) }
+      }, viewPermission => {
+        Permissions.add(new Permission(viewPermission.Name, viewPermission.Description)).map( i => Ok(i.toString) )
+      }
+    )
   }
 }
--- authentication/test/controllers/AuthenticationAPIv1Spec.scala
+++ authentication/test/controllers/AuthenticationAPIv1Spec.scala
@@ -2,29 +2,33 @@ package controllers
 
 import java.util.Date
 
+import database.FakeApp
 import models.UserSession
+import models.view._
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
 
 import play.api.test._
 import play.api.test.Helpers._
+import play.mvc.Http.MimeTypes
 
 
 @RunWith(classOf[JUnitRunner])
 class AuthenticationAPIv1Spec extends Specification {
+  val jsonHeaders = FakeHeaders(Seq((CONTENT_TYPE, MimeTypes.JSON)))
 
   "AuthenticationAPIv1Spec" should {
 
     "List all logged-in users" in new WithApplication{
       val list = route(FakeRequest(POST, "/view-api/get-logged-in-users")).get
       status(list) must equalTo(OK)
-      contentType(list) must beSome.which(_ == "application/json")
+      contentType(list) must beSome.which(_ == MimeTypes.JSON)
     }
 
     "Remove a logged-in user" in new WithApplication{
       val user = new UserSession(0, "test", "test@test", new Date(), new Date(), "test")
-      val remove = route(FakeRequest(POST, "/view-api/delete-session", FakeHeaders(Seq(("Content-Type", "application/json"))), user.toJson())).get
+      val remove = route(FakeRequest(POST, "/view-api/delete-session", jsonHeaders, user.toJson())).get
 
       status(remove) must equalTo(OK)
     }
