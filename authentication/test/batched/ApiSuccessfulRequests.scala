fa4292e4457d4bf2e59e16ab7821f3b1406ccd61
RichyHBM <gitemail@richyhbm.co.uk>
2016-01-18 19:11:51 +0000

    Add tests to get all apps, groups, users and permissions

--- authentication/test/batched/ApiSuccessfulRequests.scala
+++ authentication/test/batched/ApiSuccessfulRequests.scala
@@ -3,13 +3,18 @@ package batched
 import controllers.routes
 import models.NewUser
 import models.view._
+import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import utilities._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 //Due to a ThreadExecutor issue during unit testing,
 // all API calls that interact with Slick need to be run together and before any other test
 class ApiSuccessfulRequests extends APIBatchRunner[ApiSuccessfulRequests] {
+
+  //<editor-fold desc="App Tests">
+
   def AddNewApp = {
     val v = new ViewApp(0, "Test", "Description")
     val add = route(FakeRequest(POST, routes.AppController.newApp().url, Statics.jsonHeaders, v.toJson())).get
@@ -21,6 +26,19 @@ class ApiSuccessfulRequests extends APIBatchRunner[ApiSuccessfulRequests] {
     val edit = route(FakeRequest(POST, routes.AppController.editApp().url, Statics.jsonHeaders, e.toJson())).get
     status(edit) must equalTo(OK)
   }
+
+  def GetAllApps = {
+    val response = route(FakeRequest(POST, routes.AppController.getAllApps().url)).get
+    status(response) must equalTo(OK)
+    Json.fromJson[Array[ViewApp]](contentAsJson(response)).fold(
+      e => 1 must equalTo(0),
+      ar => ar.length must equalTo(1)
+    )
+  }
+
+  //</editor-fold>
+
+  //<editor-fold desc="Group Tests">
 
   def AddNewGroup = {
     val v = new ViewGroup(0, "Test", "Description")
@@ -34,6 +52,19 @@ class ApiSuccessfulRequests extends APIBatchRunner[ApiSuccessfulRequests] {
     status(edit) must equalTo(OK)
   }
 
+  def GetAllGroups = {
+    val response = route(FakeRequest(POST, routes.GroupController.getAllGroups().url)).get
+    status(response) must equalTo(OK)
+    Json.fromJson[Array[ViewGroup]](contentAsJson(response)).fold(
+      e => 1 must equalTo(0),
+      ar => ar.length must equalTo(1)
+    )
+  }
+
+  //</editor-fold>
+
+  //<editor-fold desc="Permission Tests">
+
   def AddNewPermission = {
     val v = new ViewPermission(0, "Test", "Description")
     val add = route(FakeRequest(POST, routes.PermissionController.newPermission().url, Statics.jsonHeaders, v.toJson())).get
@@ -46,9 +77,34 @@ class ApiSuccessfulRequests extends APIBatchRunner[ApiSuccessfulRequests] {
     status(edit) must equalTo(OK)
   }
 
+  def GetAllPermissions = {
+    val response = route(FakeRequest(POST, routes.PermissionController.getAllPermissions().url)).get
+    status(response) must equalTo(OK)
+    Json.fromJson[Array[ViewPermission]](contentAsJson(response)).fold(
+      e => 1 must equalTo(0),
+      ar => ar.length must equalTo(1)
+    )
+  }
+
+  //</editor-fold>
+
+  //<editor-fold desc="User Tests">
+
   def AddNewUser = {
     val u = new NewUser("John Doe", "test@test.test", "Password", "Password")
     val add = route(FakeRequest(POST, routes.UserController.newUser().url, Statics.jsonHeaders, u.toJson())).get
     status(add) must equalTo(OK)
   }
+
+  def GetAllUsers = {
+    val response = route(FakeRequest(POST, routes.UserController.getAllUsers().url)).get
+    status(response) must equalTo(OK)
+    Json.fromJson[Array[ViewUser]](contentAsJson(response)).fold(
+      e => 1 must equalTo(0),
+      ar => ar.length must equalTo(1)
+    )
+  }
+
+  //</editor-fold>
+
 }
